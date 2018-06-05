9f11a3114178945f027dac28cba9af2c3945f363
inoyyth <supri170845@gmail.com>
2018-06-05 07:03:19 +0700

    initial master file

---
+++ database/migrations/2018_06_03_150851_create_books_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->unique();
+            $table->string('author');
+            $table->string('publisher');
+            $table->string('isbn');
+            $table->string('description');
+            $table->float('price');
+            $table->date('tgl_terbit');
+            $table->text('images');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
